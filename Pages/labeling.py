d2352965bf76c57b6a92de04cfe22e85c7e2eda7
shannon-you <bree@ol.edu.kg>
2025-10-24 15:27:40 +0800

    Delete Pages directory

--- Pages/labeling.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-
-def app():
-    st.header("标注")
-    with st.container():
-        st.subheader("标注工具-->Make Sense")
-        st.write("[立即使用>](https://www.makesense.ai/)")
-
